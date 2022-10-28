c115498154f86e0af2533e41eeeeaceb015bcac1
Ivan Boyanov <Theflowzon3@gmail.com>
2022-10-28 20:03:26 +0300

    state-demo

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ export default function App() {
     <div className="container">
       <div className="row mt-5">
         <div className="col"></div>
-        <div className="col-xs-5"> <LoginForm /> </div>
+        <div className="col-xs-5"> <LoginForm title="Логин1"/> </div>
+        <div className="col-xs-5"> <LoginForm title="Логин2"/> </div>
         <div className="col"></div>
       </div>
   </div>
--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 
-export default function LoginForm() {
+export default function LoginForm(props) {
+
+  const [title, updateTitle] = useState(props.title)
+
   return (
-    <div className="container login-form-bg">
-      <form>
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">Email address</label>
-           <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+    <>
+    <h1>{title}</h1>
+      <div className="container login-form-bg">
+        <div>
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+          </div>
+          <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1"/>
+          </div>
+          <button class="btn btn-primary" onClick={ () => { updateTitle("novo") } }>Submit</button>
         </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">Password</label>
-          <input type="password" class="form-control" id="exampleInputPassword1"/>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </form>
-    </div>
+      </div>
+    </>
   )
 }
