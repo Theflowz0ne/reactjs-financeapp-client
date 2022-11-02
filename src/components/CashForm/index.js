01f95641b12baa6834263e3972e4bd640f3eaab9
Ivan Boyanov <Theflowzon3@gmail.com>
2022-11-03 00:02:28 +0200

    add broken income

--- src/components/CashForm/index.js
+++ src/components/CashForm/index.js
@@ -1,12 +1,29 @@
+import { request } from "../../network-client";
+import { useState } from "react";
+
 export default function CashForm(props){
+    const [amount, setAmount] = useState([]);
+    const [comment, setComment] = useState([]);
+
+    const addValues = ()=>{
+        request("post", "income/add", undefined, {
+            date: "2022-10-09T13:50:00.000Z",
+            amount: amount,
+            comment: comment
+        });
+    }
 return <>
 <div className="container my-3">
     <div className="row mt-3">
         <div className="col">
-            <input type="text" className="form-control" placeholder="Сума"/>
+            <input type="text" className="form-control" placeholder="Сума" onChange={(e)=>{
+                setAmount(e.target.value);
+            }}/>
         </div>
         <div className="col">
-            <input type="text" className="form-control" placeholder="Причина"/>
+            <input type="text" className="form-control" placeholder="Причина" onChange={(e)=>{
+                setComment(e.target.value);
+            }}/>
         </div>
     </div>
     <div className="row mt-3">
@@ -14,7 +31,7 @@ return <>
             <input type="date" className="form-control" placeholder="Дата"/>
         </div>
         <div className="col">
-            <button type="button" className="btn btn-primary form-control">Добави</button>
+            <button type="button" className="btn btn-primary form-control" onClick={addValues}>Добави</button>
         </div>
     </div>
 </div>
--- src/network-client/index.js
+++ src/network-client/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL = "https://financeapp-be.vercel.app";
 
 export const request = (method, endpoint, queryParams, requestBody) => {
-    const jwtToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjkzNzk5Nzg1LWI3NGItNGFhNC04ZjBmLTEwZTcxMjIxMzNlYyIsImVtYWlsIjoiaXZhbkBhZG1pbi5hZG1pbiIsIm5hbWUiOiJpdmFuIiwiaXNBZG1pbiI6dHJ1ZSwiaWF0IjoxNjY3NDIyMjY4LCJleHAiOjE2Njc0MjU4Njh9.s3WVA81Krw7buTDO6_8TGQiH-9kwTfHrDhMbbwcJRt0";
+    const jwtToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjkzNzk5Nzg1LWI3NGItNGFhNC04ZjBmLTEwZTcxMjIxMzNlYyIsImVtYWlsIjoiaXZhbkBhZG1pbi5hZG1pbiIsIm5hbWUiOiJpdmFuIiwiaXNBZG1pbiI6dHJ1ZSwiaWF0IjoxNjY3NDI2MzU2LCJleHAiOjE2Njc0Mjk5NTZ9.5lup8Z_FBr1jtngmW9da43vSGwH_kM9QesqrUIBzet0";
 
     const sentRequest = axios.request( 
         {
